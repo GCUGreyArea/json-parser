5d181f3d5735205e2c59f449e085b32d74d7dae3
Barry Robinson <barry.w.robinson64@gmail.com>
2025-01-01 12:43:22 +0000

    remove externs and use new interface

--- main/main.c
+++ main/main.c
@@ -2,18 +2,10 @@
 #include "json_func.h"
 #include <signal.h>
 
-extern char * string;
-extern struct json_token_list * list;
-extern struct stack * token_stack;
-extern int poped;
-extern int pushed;
-extern int depth;
-extern int count;
-
-void sigint_handler() {
-    printf("pushed: %d, poped: %d, depth: %d\n", pushed, poped, depth);
-    printf("list->idx=%d\n",get_token_list_size(list));
-}
+// void sigint_handler() {
+//     printf("pushed: %d, poped: %d, depth: %d\n", pushed, poped, depth);
+//     printf("list->idx=%d\n",get_token_list_size(list));
+// }
 
 static char * read_file(char * name) {
     char * buffer = NULL;
@@ -39,36 +31,32 @@ static char * read_file(char * name) {
 
 
 void main(int argc, char** argv) {
-    signal(SIGINT,sigint_handler);  
-    signal(SIGSEGV,sigint_handler);
+    // signal(SIGINT,sigint_handler);  
+    // signal(SIGSEGV,sigint_handler);
 
-    printf("Running\n");
+    // printf("Running\n");
     if(argc == 1) {
         printf("Please supply a JSON string to parse\n");
         exit(1);
     }
 
     if(argc == 3 && strcmp(argv[1],"-f") == 0) {
-        string = read_file(argv[2]);
+        char * string = read_file(argv[2]);
         if(string) {
-            printf("made string\n");
-
-            list = make_tokens(1024);
-            token_stack = make_stack(1024);
-            printf("made token_stack\n");
+            init_parser(1024);
             parse_string_bison(string);
-            printf("parsed string\n");
-            iterate_list(list,string);
-            printf("Done...\n");
-            delete_list(list);
-
+            print_list(string);
+            free_parser();
             free(string);        
         }
 
         exit(1);
     }
     else {
-        string = argv[1];
+        char * string = argv[1];
+        init_parser(1024);
         parse_string_bison(string);
+        print_list(string);
+        free_parser();
     }
 }
